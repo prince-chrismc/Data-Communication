a94073a1f7192446cab4b2828dd6556dd232e9a9
Chris McArthur <prince.chrismc@gmail.com>
2018-10-31 09:51:14 -0400

    removing windows struct for marco subsitution

--- Assignments/Text-Protocol/src/Socket.cpp
+++ Assignments/Text-Protocol/src/Socket.cpp
@@ -6,7 +6,7 @@ bool TextProtocol::Socket::Open( const char * pAddr, uint16 nPort )
 {
    CActiveSocket::Open( pAddr, nPort );
 
-   m_ServerIp = IpV4Address{ m_stServerSockaddr.sin_addr.S_un.S_addr };
+   m_ServerIp = IpV4Address{ m_stServerSockaddr.sin_addr.s_addr };
 
 
    return false;
